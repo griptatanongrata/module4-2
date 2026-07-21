caf21c8ec3d93402fbe6efb55b0a8d52a016f135
griptatanongrata <j.ttipton@yahoo.com>
2026-07-21 15:40:07 -0500

    Module 4 Part 2

--- README.md
+++ README.md
@@ -9,3 +9,25 @@ _Definition_
 -creating conditions to allow access to certain information
 -Utilizes "if", "else if", and "else", combined with "&&" and "||" to meet a ternary (one-line shortcut)
 -Examples shown in index.js
+
+_Comparison Operators_
+
+-mentioned before. ===, ==, !=, !==, >=, <=, etc...
+-REMEMBER!: an ! can be attached to a variable as well. It just signals for the OPPOSITE of the variable of operator.
+
+_Logical Operators_
+
+-&&: checks if both the left and right side of the comparison is true
+-||: checks to see if either the left or right side of the comparison is true
+
+_Truthy & Falsy Values_
+
+-truthy values are responses that give back a variable of any data type in an if statement
+-falsy values return "undefined, empty string, null, NaN (not a number), 0, or false
+-the truthy value should return in the if statement
+-falsy values can come through in the else
+
+_Ternary Operators_
+
+-a shortcut utilizing ? for true and : for false
+-examples in index.js
--- index.js
+++ index.js
@@ -10,3 +10,44 @@ if (subscribed === true) {
 } else {
   console.log("Log into your account");
 }
+
+let cash = 50;
+let price = 40;
+let difference = cash - price;
+
+if (cash > price) {
+  console.log(`here is your change of ${difference} dollars`);
+} else if (cash === price) {
+  console.log("you paid the exact amount, have a nice day");
+} else {
+  console.log(`not enough money - you still owe ${difference * -1} dollars`);
+}
+
+// Utilizing Logical Operators //
+
+let isStoreOpen = true;
+
+if (cash >= price && isStoreOpen === true) {
+  console.log(`print the receipt`);
+} else {
+  console.log(`you are unable to make a purchase`);
+}
+
+// Truthy & Falsy Values //
+
+let val = "Justin";
+
+if (val) {
+  console.log(!!val);
+} else {
+  console.log(!!val);
+}
+
+// Ternary Operators //
+
+let hot = true;
+
+hot ? console.log(`weather is hot outside`) : console.log(`weather is cold`);
+
+let storeTrip = cash >= price && isStoreOpen ? `give receipt` : `no receipt`;
+console.log(storeTrip);
